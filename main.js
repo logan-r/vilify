15f91bb4e9a7b88a0584ff4f820478f15463541f
Logan <kai.ralston@gmail.com>
2013-01-17 11:38:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@
 
 //Game Constants
 var FPS = 20; //The number of frames per second (the amount of time for second that things get updated)
-var ctx = document.getElementById('canvas').getContext('2d');
+var stage = document.getElementById('canvas').getContext('2d'); //create a variable stage to draw upon
 
 function Update() {
-    //Updates the game objects
+    //TODO: Update the game objects
     
-    //Draws the game objects
+    //Draw the game objects
     Draw();
 }
 
 function Draw() {
-    //Draws the game objects on the canvas
+    //TODO: Draw the game objects on the canvas
 }
 
 setInterval(Update, 1000/FPS); //Updates the game FPS times per second
