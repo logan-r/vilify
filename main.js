335c45ba862e6b3f0a28d9d38d3608e0ac19c26e
Logan <kai.ralston@gmail.com>
2013-01-17 10:54:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 //vilify's main js file
+
+//Game Constants
+FPS = 20; //The number of frames per second (the amount of time for second that things get updated)
+
+function Update() {
+    //Updates the game objects
+    
+    //Draws the game objects
+    Draw();
+}
+
+function Draw() {
+    //Draws the game objects on the canvas
+}
+
+setInterval(Update, 1000/FPS); //Updates the game FPS times per second
