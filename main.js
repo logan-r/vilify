96c452e858a59cdf61c1f43d891bb9ec461d8842
SungRim Huh <sungrimhuh@gmail.com>
2013-02-11 17:26:29 -0600

    Update main.js
    Separate object constructors from regular functions
    add function tick
    add tileLength

--- main.js
+++ main.js
@@ -4,39 +4,16 @@
 var FPS = 20; //The number of frames per second (the amount of time for second that things get updated)
 var stage = document.getElementById('canvas').getContext('2d'); //create a variable stage to draw upon
 var Map; //The game map
+var tileLength = 64; //The tile length (since it is awkward to call it a width or height)
 
-//object creators
+//object constructors
 
-fucntion Tower(){ //tower object creator
+fucntion Tower() { //tower object creator
     //TODO: Define some basic attributs that all towers can inherit        
 }
 
-function Monster(){ //monster object creator
+function Monster() { //monster object creator
     //TODO: Define some basic attributes that all monsters can inherit
-}
-
-function Update() {
-    //TODO: Update the game objects
-    
-    //Draw the game objects
-    Draw();
-}
-
-function Draw() {
-    //Clear stage so we can draw over it
-    stage.clearRect(0,0,640,452);
-    
-    //Draw background (currently just a solid color)
-    stage.fillStyle = "black";
-    stage.fillRect(0,0,640,452);
-    
-    //Draw map
-    Map.draw();
-    
-    //Draw menu
-    stage.font = "40px Snowburst One";
-    stage.fillStyle = "white";
-    stage.fillText("Vilify",485,50);
 }
 
 function GameMap(_map) {
@@ -47,17 +24,17 @@ function GameMap(_map) {
     */
     this._map = _map;
     this.draw = function() {
-        for (row=0; row<this._map.length; row++) { //Loop through the rows
-            for (column=0; column<this._map[row].length; column++) { //Loop through the column
+        for (var row = 0; row < this._map.length; row++) { //Loop through the rows
+            for (var column = 0; column < this._map[row].length; column++) { //Loop through the columns
                 //get tile color
-                if (this._map[row][column] == 1) { //on path tile
+                if (this._map[row][column] === 1) { //on path tile
                     stage.fillStyle = "white";
                 } else { //not on path tile
                     stage.fillStyle = "grey";
                 }
                 
                 //draw a 64x64 tile in the correct location
-                stage.fillRect(row*64+2, column*64+2, 64, 64);
+                stage.fillRect(row * tileLength + 2, column * tileLength + 2, tileLength, tileLength);
             }
         }
     }
@@ -74,5 +51,33 @@ Map = new GameMap([
 [1,0,1,0,1,0,1]
 ]);
 
-//Create a timer that calls a function, Update (which updates the game), FPS times per second
-setInterval(Update, 1000/FPS);
+//Global functions
+
+function update() {
+    //TODO: Update the game objects
+}
+
+function draw() {
+    //Clear stage so we can draw over it
+    stage.clearRect(0, 0, stage.width, stage.height);
+    
+    //Draw background (currently just a solid color)
+    stage.fillStyle = "black";
+    stage.fillRect(0, 0, stage.width, stage.height);
+    
+    //Draw map
+    Map.draw();
+    
+    //Draw menu
+    stage.font = "40px Snowburst One";
+    stage.fillStyle = "white";
+    stage.fillText("Vilify", 485, 50);
+}
+
+function tick() {
+    update();
+    draw();
+}
+
+//Create a timer that calls a function, tick (which updates the game and draw), FPS times per second
+setInterval(tick, 1000/FPS);
