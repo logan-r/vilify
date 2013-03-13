e95e5d383e01288974a12ebe10a7b19609fb5fff
SungRim Huh <sungrimhuh@gmail.com>
2013-03-13 09:25:53 -0500

    Refactor map_creator.js

--- js/map_creator.js
+++ js/map_creator.js
@@ -1,12 +1,12 @@
-var canvas=null, ctx=null;
-var time=0,acumDt=0,interval=10,FPS=0;
-var xscale=1,yscale=1;
-var mousex=-1,mousey=-1;
-var timer=0;
+var canvas = null, ctx = null;
+var time = 0, acumDt = 0, interval = 10, FPS = 0;
+var xscale = 1, yscale = 1;
+var mousex =- 1, mousey =- 1;
+var timer = 0;
 var map;
 var matAux;
-var max=50;
-var puntos=0;
+var max = 50;
+var puntos = 0;
 var mouseFlag = 0;
 
 var start = new Image();
@@ -22,27 +22,27 @@ $( document ).ready( function() {
 	$( "#i_width" ).change( function() {
 		map.w = $( this ).val();
 		recalculateScale();
-	});
+	} );
 	$( "#i_height" ).change( function() {
 		map.h=$( this ).val();
 		recalculateScale();
-	});
+	} );
 
 	$( window ).on( "mousedown", function mouseClick( evt ) {
 		mouseDown( evt );
 		mouseFlag = 1;
-	});
+	} );
 	$( window ).on( "mouseup", function() {
 		mouseFlag = 0;
-	});
+	} );
 	$( window ).on( "keydown", function keyDown( evt ) {
 		if ( evt.keyCode == 83 ) { // S
 			map.set( mousex, mousey, 2 );
 		}
 		if ( evt.keyCode == 69 && !map.isEnd() ) { // E
-			map.set( mousex,mousey, 3 );
+			map.set( mousex, mousey, 3 );
 		}
-	});
+	} );
 	
 	$( window ).on( "mousemove", function mouseMove( evt ) {
 		var mousexx = evt.pageX - canvas.offsetLeft;
@@ -54,55 +54,55 @@ $( document ).ready( function() {
 		mousexx=parseInt( ( ( mousexx / xscale ) - xcorrection ).toFixed( 0 ) ); //parseInt for deny the -0 demon.
 		mouseyy=parseInt( ( ( mouseyy / yscale ) - ycorrection ).toFixed( 0 ) );
 		
-		if ( map.isInside( mousexx,mouseyy ) ) {
-			mousex=mousexx;
-			mousey=mouseyy;
+		if ( map.isInside( mousexx, mouseyy ) ) {
+			mousex = mousexx;
+			mousey = mouseyy;
 		}
 		
 		if ( mouseFlag == 1 ) {
 			//drag
-			mouseDown(evt);
+			mouseDown( evt );
 		}
-	});
+	} );
 
-	canvas=document.getElementById( 'canvas' );
+	canvas = document.getElementById( 'canvas' );
 	canvas.style.background = '#000';
-	ctx=canvas.getContext( '2d' );
+	ctx = canvas.getContext( '2d' );
 	canvas.addEventListener( "mouseout", function mouseOut( evt ) {
-		mousex=-1;
-		mousey=-1;
-	});
+		mousex =- 1;
+		mousey =- 1;
+	} );
 	canvas.onselectstart = function () { return false; } //Prevents double-click selection.
 	$( "#canvas" ).mousedown( function( event ){ //Prevents Drag selection.
 		event.preventDefault();
-	});
+	} );
 	
 	//Init
 	objetosInit();
 	
 	//
 	run();
-});
+} );
 function recalculateScale() {
 	xscale = canvas.width / map.w;
 	yscale = canvas.height / map.h;
 }
 function objetosInit() {
-	rectangulos = new Array();
+	// rectangulos = new Array();
 	map = new Map( $( "#i_width" ).val(), $( "#i_height" ).val() );	
 	recalculateScale();	
 }
 function run() {
-	//setTimeout(run,50);
+	// setTimeout(run, 50);
 	requestAnimFrame( run );
-	var now=new Date().getTime();
+	var now = new Date().getTime();
 	var dt = now - ( time || now );
 	FPS = 1000 / dt;
 	time = now;
 	acumDt += dt;
 	 
 	while ( acumDt > interval ) {
-		acumDt-=interval;
+		acumDt -= interval;
 	}
 	paint( ctx );
 }
@@ -134,7 +134,7 @@ function paint( ctx ) {
 			ctx.drawImage( img, j * xscale, i * yscale, xscale, yscale );
 			//bordes
 			ctx.strokeStyle = '#888';
-			ctx.strokeRect( j * xscale, i * yscale, xscale, yscale);			
+			ctx.strokeRect( j * xscale, i * yscale, xscale, yscale );			
 		}
 	}	
 	
@@ -155,9 +155,9 @@ function getMapArrayCode() {
 	for ( var i = 0; i < map.h; i++ ) {
 		m += tab;
 		for ( var j = 0; j < map.w; j++ ) {
-			if ( j == 0 ) m += '[';
+			if ( j === 0 ) m += '[';
 			m = m + map.get( j, i );
-			if ( j == map.w - 1 ) {
+			if ( j === map.w - 1 ) {
 				m += "]";
 				if ( i < map.h - 1 ) m += ",";
 				m += '<br>';
@@ -210,18 +210,18 @@ function mouseDown( evt ) {
 			case 2:
 				break;
 			case 3:
-				map.set(mousex,mousey,0);
+				map.set( mousex, mousey, 0 );
 				break;
 		}
 	}
 }
 
-window.requestAnimFrame = (function(){
+window.requestAnimFrame = ( function() {
  return window.requestAnimationFrame || 
   window.webkitRequestAnimationFrame || 
   window.mozRequestAnimationFrame || 
   function( callback ) { window.setTimeout( callback, 17 ); };
-})();
+} )();
 
 
 function Map( w, h ) {
