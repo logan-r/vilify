e2294bebfa6b6e0d25c4648f1823ae4d78791227
Logan <kai.ralston@gmail.com>
2013-03-16 19:00:14 -0300

    Split code
    Moved loading code to load.js.
    Moved Vilify specific objects to objects.js.

--- js/main.js
+++ js/main.js
@@ -7,42 +7,6 @@
 // Our canvas context for drawing
 var stage = window.stage = settings.stage = settings.canvas.getContext( "2d" );
 
-// Create asset manager
-Game.assetManager = new AssetManager();
-
-// Fetch object data
-Game.assetManager.addAsset( "objects.json", "json", "game_data/objects.json", function( data ) {
-	settings.objectData = data;
-});
-
-// Fetch map data
-Game.assetManager.addAsset( "maps.json", "json", "game_data/maps.json", function( data ) {
-	settings.mapData = data;
-	settings.map = new GameMap( data.map1.mapArray );
-	settings.map.waves = data.map1.waves;
-});
-
-// Fetch tile sprite sheet data
-Game.assetManager.addAsset( "tiles.json", "json", "images/tiles.json", function( data ) {
-	settings.tileData = data;
-});
-
-// Fetch tower sprite sheet data
-Game.assetManager.addAsset( "towers.json", "json", "images/towers.json", function( data ) {
-	settings.towerData = data;
-});
-
-// Add files to asset manager
-Game.assetManager.addAsset( "Tile Sprite Sheet", "image", "images/tiles.png" );
-Game.assetManager.addAsset( "Tower Sprite Sheet", "image", "images/towers.png" );
-
-// Global functions
-
-buildTower = function (type, x, y) {
-	tower = new Tower( type, { x: (64 * x) + 5, y: (64 * y) + 5, width: 64, height: 64 } );
-	settings.entities.push( tower );
-}
-
 /**
  * Updates the game state
  */
@@ -66,10 +30,6 @@ Game.draw = function() {
 	// Clear stage so we can draw over it
 	stage.clearRect( 0, 0, stage.width, stage.height );
 
-	// Draw background (currently just a solid color)
-	stage.fillStyle = "black";
-	stage.fillRect( 0, 0, stage.width, stage.height );
-
 	// Draw map
 	settings.map.draw();
 	
@@ -87,8 +47,8 @@ Game.tick = function() {
 	Game.draw();
 }
 
-// Load images and start game when done
-Game.assetManager.load( function() {
+// Called when game starts
+var startGame = function() {
 	// Create a timer that calls a function, tick (which updates the game and draw), FPS times per second
 	setInterval( Game.tick, 1000 / settings.FPS );
 	
@@ -101,7 +61,9 @@ Game.assetManager.load( function() {
 	buildTower( "Poison Tower", 7, 7 );
 	buildTower( "Lightning Tower", 5, 1 );
 	buildTower( "Curse Tower", 8, 3 );
-});
+};
+
+window.startGame = startGame;
 
 // Resize the canvas when the window is resized
 // UPDATE: Test this later. Let's get the game running first
