e7bc7fb37a85f3c674f8439099dfe81f2b7bb0d3
Logan <kai.ralston@gmail.com>
2013-03-19 22:10:32 -0300

    Add basic lab and inventory

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,7 +6,14 @@
 
 var Game = window.Game,
 	document = window.document,
-
-	sidebar = document.getElementById( "sidebar" );
+	
+	$sidebar = $( "#sidebar" )
+	
+	// Put material in lab box
+	$( "#lab" ).children().eq( 0 ).css( "background", "grey url( image_url_here )" );
+	
+	// Put potions/towers/monsters in inventory boxes
+	$( "#inventory" ).children().eq( 0 ).children().eq( 0 ).css( "background", "grey url( image_url_here )" );
+	$( "#inventory" ).children().eq( 0 ).children().eq( 1 ).css( "background", "grey url( image_url_here )" );
 
 })( window );
