e92e62591e211761981ee6c298523d8675bf03ef
Logan <kai.ralston@gmail.com>
2013-10-29 19:00:52 -0400

    Add graveyard

--- js/monsters.js
+++ js/monsters.js
@@ -27,4 +27,30 @@
 	}
 
 	window.Monster = Monster;
+
+	/**
+	 * Graveyard object
+	 * Where monsters spawn
+	 */
+	var Graveyard = function() {
+		this.initialize();
+	}
+
+	var p = Graveyard.prototype = new createjs.Container();
+	Graveyard.prototype.Container_initialize = p.initialize;
+
+	Graveyard.prototype.initialize = function() {
+		this.Container_initialize();
+
+		// Monster data
+		this.type = null;
+
+		// Monster image
+		var image = new createjs.Shape();
+		image.graphics.beginFill("#d61500").drawRect(700, 620, 120, 20);
+		this.addChild(image);
+	}
+
+	window.Graveyard = Graveyard;
+	window.GRAVEYARD = null;
 })(window);
