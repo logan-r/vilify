1aa81caf4de5fdd2cedd7278c8b9bd9861d97c8d
Logan <kai.ralston@gmail.com>
2013-10-28 21:26:40 -0400

    Load hero data from objects.js and add flying heroes

--- js/heroes.js
+++ js/heroes.js
@@ -7,27 +7,35 @@
 	 * Hero object
 	 * Store all the data associated with one hero
 	 */
-	var Hero = function() {
-		this.initialize();
+	var Hero = function(type) {
+		this.initialize(type);
 	}
 
 	var p = Hero.prototype = new createjs.Container();
 	Hero.prototype.Container_initialize = p.initialize;
 
-	Hero.prototype.initialize = function() {
+	Hero.prototype.initialize = function(type) {
 		this.Container_initialize();
 
 		// Hero data
-		this.type = null;
+		this.type = type;
+		this.speed = GAME_DATA["heroes"][this.type]["speed"];
+		this.flying = GAME_DATA["heroes"][this.type]["flying"];
+
+		// Calculate hero starting y value
+		var y = 530;
+		if (this.flying) {
+			y = MathEx.randInt(120, 420);
+		}
 
 		// Hero image
 		var image = new createjs.Shape();
-		image.graphics.beginFill("#54eb46").drawRect(0, 530, 70, 100);
+		image.graphics.beginFill("#54eb46").drawRect(-70, y, 70, 100);
 		this.addChild(image);
 
 		// Update function
 		this.tick = function(event) {
-			hero.x += event.delta/1000*100;
+			hero.x += event.delta/1000*this.speed;
 		}
 	}
 
