743d3248ee84e2f549059be348e637dac249fece
logan-r <kai.ralston@gmail.com>
2014-12-27 00:37:43 +0000

    Create monster MVC class

---
+++ js/monsters.js
@@ -0,0 +1,42 @@
+// Monster class
+function Monster(game, init_type) {
+    /**
+     * Monster data/model
+     */
+    var model = {};
+    
+    // Categorization data
+    model.type = init_type; // The type of the monster (e.g. "alien warlord", "scrapyard robot")
+
+    // Combat data
+    model.health; // The monster's health
+    model.maxHealth; // The maximum health the monster can have
+    
+    // Movement data
+    model.speed; // The speed at which the monster move
+    
+    
+    /**
+     * Monster sprite/view
+     */
+    var view = game.add.sprite(0, 0, init_type);
+    
+    // Set the sprite's anchor point to the center of the sprite
+    view.anchor.setTo(0.5, 0.5);
+    
+    
+    /**
+     * Monster actions/controller
+     */
+    var controller = {};
+    
+    /**
+     * Generate object that is an instance of this class
+     */
+    var _public = {
+        "type": "monster",
+        "m": model,
+        "v": view,
+        "c": controller
+    }; return _public;
+}
