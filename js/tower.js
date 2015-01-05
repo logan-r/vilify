22c6c485ff0e6ebb9ca7795ccc3c6a5a51c98c2d
SungRim Huh <sungrimhuh@gmail.com>
2015-01-04 22:05:13 -0600

    Add Tower class. Closes #87

---
+++ js/tower.js
@@ -0,0 +1,32 @@
+// Tower class
+function Tower(game, type) {
+    /**
+     * Tower data/model
+     */
+    var model = {
+        type: type
+    };
+
+    /**
+     * Tower sprite/view
+     */
+    var view = game.add.sprite(0, 0, type);
+
+    // Set the sprite's anchor point to the center of the sprite
+    view.anchor.setTo(0.5, 0.5);
+
+
+    /**
+     * Tower actions/controller
+     */
+    var controller = {};
+
+    /**
+     * Generate object that is an instance of this class
+     */
+    return {
+        m: model,
+        v: view,
+        c: controller
+    };
+}
