b085cb4ae300230c7ebf668e338a142ef8cdada7
logan-r <kai.ralston@gmail.com>
2015-01-06 08:27:21 +0000

    Add GameObject class and have Item inherit from it

--- js/item.js
+++ js/item.js
@@ -1,25 +1,23 @@
 // Item class
 function Item(game, type) {
+    // Inherits from GameObject
+    var _superclass = GameObject(game, type);
+    
     /**
      * Item data/model
      */
-    var model = {
-        type: type
-    };
+    var model = _superclass.m;
 
     /**
      * Item sprite/view
      */
-    var view = game.add.sprite(0, 0, type);
-
-    // Set the sprite's anchor point to the center of the sprite
-    view.anchor.setTo(0.5, 0.5);
+    var view = _superclass.v;
 
 
     /**
      * Item actions/controller
      */
-    var controller = {};
+    var controller = _superclass.c;
 
     /**
      * Generate object that is an instance of this class
---
+++ js/superclasses.js
@@ -0,0 +1,35 @@
+/**
+ * GameObject class
+ * Base class for anything in the game world
+ * Parent class of Item and PhysicalObject
+ */
+function GameObject(game, type) {
+    /**
+     * Game object data/model
+     */
+    var model = {
+        type: type
+    };
+    
+    /**
+     * Game object sprite/view
+     */
+    var view = game.add.sprite(0, 0, type);
+    
+    // Set the sprite's anchor point to the center of the sprite
+    view.anchor.setTo(0.5, 0.5);
+    
+    /**
+     * Game object actions/controller
+     */
+    var controller = {};
+    
+    /**
+     * Generate object that is an instance of this class
+     */
+    return {
+        m: model,
+        v: view,
+        c: controller
+    };
+}
