6e79c72b4e40bdbe4856326d1ce9f7f4fa29a265
logan-r <kai.ralston@gmail.com>
2015-01-06 08:44:27 +0000

    Add skeleton code for PhysicalObject, AnimateObject & FightingObject superclasses #90

--- js/superclasses.js
+++ js/superclasses.js
@@ -5,14 +5,14 @@
  */
 function GameObject(game, type) {
     /**
-     * Game object data/model
+     * GameObject data/model
      */
     var model = {
         type: type
     };
     
     /**
-     * Game object sprite/view
+     * GameObject sprite/view
      */
     var view = game.add.sprite(0, 0, type);
     
@@ -20,7 +20,7 @@ function GameObject(game, type) {
     view.anchor.setTo(0.5, 0.5);
     
     /**
-     * Game object actions/controller
+     * GameObject actions/controller
      */
     var controller = {};
     
@@ -32,5 +32,114 @@ function GameObject(game, type) {
         v: view,
         c: controller,
         type: "GameObject"
+    };
+}
+
+
+/**
+ * PhysicalObject class
+ * Base class for any physical object in the game world
+ * Parent class of Tower and AnimateObject
+ */
+function PhysicalObject(game, type) {
+    // Inherits from GameObject
+    var _superclass = GameObject(game, type);
+    
+    /**
+     * PhysicalObject data/model
+     */
+    var model = _superclass.m;
+    
+    /**
+     * PhysicalObject sprite/view
+     */
+    var view = _superclass.v;
+    
+    /**
+     * PhysicalObject actions/controller
+     */
+    var controller = _superclass.c;
+    
+    /**
+     * Generate object that is an instance of this class
+     */
+    return {
+        m: model,
+        v: view,
+        c: controller,
+        type: "PhysicalObject"
+    };
+}
+
+
+/**
+ * AnimateObject class
+ * Base class for any physical, moving object in the game world
+ * Parent class of Projectile and FightingObject
+ */
+function AnimateObject(game, type) {
+    // Inherits from PhysicalObject
+    var _superclass = PhysicalObject(game, type);
+    
+    /**
+     * AnimateObject data/model
+     */
+    var model = _superclass.m;
+    
+    /**
+     * AnimateObject sprite/view
+     */
+    var view = _superclass.v;
+    
+    /**
+     * AnimateObject actions/controller
+     */
+    var controller = _superclass.c;
+    
+    /**
+     * Generate object that is an instance of this class
+     */
+    return {
+        m: model,
+        v: view,
+        c: controller,
+        type: "AnimateObject"
+    };
+}
+
+
+/**
+ * FightingObject class
+ * Base class for any physical, moving object in the game world that can enter
+ * direct combat with other objects
+ * Parent class of Hero and Monster
+ */
+function FightingObject(game, type) {
+    // Inherits from AnimateObject
+    var _superclass = AnimateObject(game, type);
+    
+    /**
+     * FightingObject data/model
+     */
+    var model = _superclass.m;
+    
+    /**
+     * FightingObject sprite/view
+     */
+    var view = _superclass.v;
+    
+    /**
+     * FightingObject actions/controller
+     */
+    var controller = _superclass.c;
+    
+    /**
+     * Generate object that is an instance of this class
+     */
+    return {
+        m: model,
+        v: view,
+        c: controller,
+        type: "FightingObject"
     };
 }
