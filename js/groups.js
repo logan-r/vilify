01aacb228357deda8f33ed15560e9a48735e105d
logan-r <kai.ralston@gmail.com>
2015-01-17 05:50:52 +0000

    Display items in inventory on right side of the screen. Closes #110

--- js/groups.js
+++ js/groups.js
@@ -1,7 +1,7 @@
 // Object group class - contains a group of other objects
 function ObjectGroup(game) {
-    // items - the list of objects in the group
-    var items = [];
+    // objs - the list of objects in the group
+    var objs = [];
     
     // views - a phaser group of sprites/views for layering/z-indexing purposes
     var views = game.add.group();
@@ -11,21 +11,60 @@ function ObjectGroup(game) {
     // Adds an object to the group
     // obj - the object to add
     controller.add = function(obj) {
-        items.push(obj);
+        objs.push(obj);
         views.add(obj.v);
     };
     
     // Iterates over every object in this object group and call its update
     // function
     controller.update = function() {
-        for (var i = 0; i < items.length; i++) {
-            items[i].c.update();
+        for (var i = 0; i < objs.length; i++) {
+            objs[i].c.update();
         }
     };
     
     // Add in items to final object for debug purposes only
     // TODO: return only controller instead of _obj
     var _obj = controller;
-    _obj.items = items;
+    _obj.objs = objs;
+    _obj.views = views;
+    return _obj;
+}
+
+// Inventory group class - contains a group of items that the player owns
+function InventoryGroup(game) {
+    // Inherits from ObjectGroup
+    var _superclass = ObjectGroup(game);
+    
+    // Globals
+    // TODO: find better place to define
+    var POS_X = game.width - 50; // the x location of the inventory
+    var POS_Y = 50; // the y location of the inventory
+    var SPACING_Y = 75; // the y-axis spacing between each item
+    
+    // Properties inherited from ObjectGroup
+    var objs = _superclass.objs;
+    var views = _superclass.views;
+    
+    var controller = _superclass;
+    
+    // Extend the add function so that it position the item in correct location
+    // inside inventory
+    controller.add = function(item) {
+        // Extend from the ObjectGroup add function
+        objs.push(item);
+        views.add(item.v);
+        
+        // But detirmin the position of the item based upon what already in the
+        // inventory
+        item.v.x = POS_X;
+        item.v.y = POS_Y + SPACING_Y * (objs.length - 1);
+    };
+    
+    // Add in items to final object for debug purposes only
+    // TODO: return only controller instead of _obj
+    var _obj = controller;
+    _obj.objs = objs;
+    _obj.views = views;
     return _obj;
 }
