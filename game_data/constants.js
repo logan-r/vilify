194146c69f5835eca7c281d0492e62b35b6a65cd
logan-r <kai.ralston@gmail.com>
2015-02-05 06:20:09 +0000

    Add constants file

---
+++ game_data/constants.js
@@ -0,0 +1,5 @@
+window.data = window.data || {}; // Make sure window.data is defined
+
+window.data.constants = {
+    "TOWER_ANGULAR_VELOCITY": 0.12 // The at which the turrets of the towers move
+};
